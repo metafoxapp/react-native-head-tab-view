c151bcca2c1b49c376619849b5133ce068bd1a4f
tamdv@younetgroup.com <39816672+Vantam1601@users.noreply.github.com>
2022-11-07 14:29:33 +0700

    Fix Flatlist android

--- Example/react-native-head-tab-view/index.js
+++ Example/react-native-head-tab-view/index.js
@@ -1,9 +1,11 @@
-import { ScrollView, FlatList, SectionList } from 'react-native'
+import { ScrollView, SectionList, FlatList as RNFlatList } from 'react-native'
 import createCollapsibleScrollView from './createCollapsibleScrollView'
 export { HeaderContext } from './HeaderContext'
 export { default as GestureContainer } from './GestureContainer'
+export { FlatList }  from 'react-native-gesture-handler'
 
 const HScrollView = createCollapsibleScrollView(ScrollView)
-const HFlatList = createCollapsibleScrollView(FlatList)
+const HFlatList = createCollapsibleScrollView(RNFlatList)
+const GHFlatList = createCollapsibleScrollView(FlatList)
 const HSectionList = createCollapsibleScrollView(SectionList)
-export { HScrollView, HFlatList, HSectionList }
+export { HScrollView, HFlatList, HSectionList, GHFlatList }
